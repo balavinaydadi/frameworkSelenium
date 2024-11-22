f451b7ac05993c8d0b2f90e65bdb6d72c7d2096b
vinay.dadi <dadibalavinay@gmail.com>
2024-11-22 12:04:53 +0530

    added one more class in the Practice for login

---
+++ src/test/java/practice/OpeningFlipkart.java
@@ -0,0 +1,36 @@
+package practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class OpeningFlipkart {
+
+	
+	@BeforeTest
+	void init() {
+		
+		System.out.println("before test executed now");
+		
+	}
+	@Test
+	void login() {
+		
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com/");
+		Thread.onSpinWait();
+		
+	}
+	@AfterTest
+	void close() {
+		
+		System.out.println("after test executed now");
+		
+
+}}
